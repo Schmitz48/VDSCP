45bff2583f8c121be784fd75d4d0c54f9361b095
Philipp Schmitz <philipp@bpma.de>
2020-12-01 22:06:00 +0100

    Created a test case for the correct initialization Expected BDD_IDs: True() 1, False() 0

--- src/test/Tests.h
+++ src/test/Tests.h
@@ -8,6 +8,22 @@
 #include "gtest/gtest.h"
 #include "../Manager.h"
 
+class ManagerTest : public ::testing::Test {
+public:
+    /** Contains tests for the Manager class
+     * a new manager object is constructed with the initialization
+    */
+    ClassProject::Manager* manager;
+    ManagerTest(): manager(new ClassProject::Manager()){}
+};
+
+TEST_F(ManagerTest, initialization) {
+    /** Check for correct initialization of the unique table
+     */
+    EXPECT_EQ(manager->False(), 0); //! False should be 0 after initialization
+    EXPECT_EQ(manager->True(), 1); //! True should be 1 after initialization
+}
+
 
 #endif //VDS_PROJECT_TESTS_H
 
